12f76d84a9cae2b769a45cabfc5eaf0eb85a53c4
Lucas Makinen <tmakinen@princeton.edu>
2019-09-26 11:12:54 -0400

    to_rearr changed

--- sim_format.py
+++ sim_format.py
@@ -80,8 +80,8 @@ if __name__ == '__main__':
 			to_rearr_inds = inds_nest[inds_in] - PIX_SELEC*NPIX_WINDOW
 			#to_rearr = fgd
 			to_rearr = fgd[inds_in]
+			to_rearr = (to_rearr[np.argsort(to_rearr_inds)])#[rearr]
 			print(to_rearr)
-			to_rearr = (to_rearr[np.argsort(to_rearr_inds)])[rearr]
 			to_rearr = np.reshape(to_rearr,(WINDOW_LENGTH,WINDOW_LENGTH,NU_AVG))
 
 			np.save("%s/run_%s_s1%03d/win%03d_%s"%(dirstr,type_str,SNUM,PIX_SELEC,type_str2),to_rearr)
